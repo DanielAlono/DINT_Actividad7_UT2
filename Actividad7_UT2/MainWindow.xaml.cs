89811f000ac597516c68a0963466d87144126dfc
Daniel Alonso <de.alonso@iesdoctorbalmis.com>
2020-10-20 12:56:41 +0200

    Agregar archivos de proyecto.

---
+++ Actividad7_UT2/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Actividad7_UT2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediumRadioButton.IsChecked = true;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            textoTextBlock.FontSize = Convert.ToInt32((sender as RadioButton).Tag.ToString());
+        }
+    }
+}
